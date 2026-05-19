972d9afe3286a7312ee2460d0741c1dead036443
theo494 <22401130@aluno.cotemig.com.br>
2026-05-19 11:44:25 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
+app = Flask(__name__)
 
-app = Flask(__name__) 
+@app.route("/")
+def questao1():
+    return render_template("questao1.html", name = "theo")
 
-@app.route('/') 
-def site():
-    return render_template("home.html")
+@app.route("/questao2")
+def questao2():
+    dados = [{"nome": "theo", "idade": 17}]
+    return render_template("questao2.html", alunos = dados)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
